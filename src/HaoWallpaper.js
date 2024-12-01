fc609c272e95968e3c5060ece610ae9b15e96cff
tobe <583590744@qq.com>
2024-12-01 17:08:09 +0800

    壁纸调整

--- src/HaoWallpaper.js
+++ src/HaoWallpaper.js
@@ -1,10 +1,8 @@
 // ==UserScript==
-// @name         HaoWallpaper Page Navigator
-// @name:zh      好壁纸页面导航
-// @name:zh-CN   好壁纸页面导航器
-// @name:en      HaoWallpaper Page Navigator
-// @description         Use arrow keys to navigate pages
-// @description:zh-CN     使用方向键切换页面
+// @name         HaoWallpaper Perf
+// @name:zh      好壁纸页面优化
+// @description  支持方向键进行导航
+// @description  优化壁纸缩放尺寸，mac一屏幕能够看完
 // @namespace    http://tampermonkey.net/
 // @version      0.0.1
 // @author       Tobe
@@ -19,6 +17,10 @@
     'use strict';
 
     var DEBUG = true;
+
+    // -- feature config --
+    var SMALL_CARD = true;
+    var SHOW_TIP = true;
 
     var pageDiv, prevButton, nextButton, valid;
 
@@ -116,24 +118,61 @@
         }
     });
 
-    // 添加提示信息
-    const tip = document.createElement('div');
-    tip.style.cssText = `
-        position: fixed;
-        bottom: 20px;
-        right: 20px;
-        background: rgba(0, 0, 0, 0.7);
-        color: white;
-        padding: 10px 15px;
-        border-radius: 5px;
-        z-index: 9999;
-        font-size: 14px;
-    `;
-    tip.textContent = '← →方向键切换页面';
-    document.body.appendChild(tip);
+    // 默认一屏幕看不下，缩小一点卡片
+    function fixCardCss() {
+        if (!SMALL_CARD) return;
+        for (let stylesheet of document.styleSheets) {
+            for (let rule of stylesheet.cssRules) {
+                if (rule.selectorText === '.TheHomeBody .card') {
+                    rule.style.width = '16%';
+                    return
+                }
+            }
+        }
+    }
+    fixCardCss();
 
-    // 3秒后隐藏提示
-    setTimeout(() => {
-        tip.style.display = 'none';
-    }, 3000);
+    // 添加提示信息
+    function addTip() {
+        if (!SHOW_TIP) return;
+        const tip = document.createElement('div');
+        tip.style.cssText = `
+            position: fixed;
+            bottom: 20px;
+            right: 20px;
+            background: rgba(0, 0, 0, 0.7);
+            color: white;
+            padding: 10px 15px;
+            border-radius: 5px;
+            z-index: 9999;
+            font-size: 14px;
+        `;
+        tip.textContent = '← →方向键切换页面';
+        document.body.appendChild(tip);
+
+        // 3秒后隐藏提示
+        setTimeout(() => {
+            // tip.style.display = 'none';
+            tip.remove();
+        }, 3000);
+    }
+    addTip();
+
+    function disableCardAni() {
+
+        // 轮询去掉 animation-duration
+        function removeAni() {
+            const cards = document.querySelectorAll('.TheHomeBody .card');
+            if (!cards) return;
+            for (let card of cards) {
+                // card.style.animationDuration = '0s';
+                card.style.animationDuration = '0.05s';
+            }
+        }
+
+        setInterval(removeAni, 50);
+    }
+
+    // NOTE: 其实图片有一个加载过程，关掉了体验不是很好
+    // disableCardAni();
 })();
